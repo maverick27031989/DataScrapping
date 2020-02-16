16e58cdc348f8b98f0b772482086dc3737cf44ba
Ajay Singh <42219497+maverick27031989@users.noreply.github.com>
2020-02-15 22:05:30 -0600

    Further Data Scrapping

--- TestDataCrawlerByStep.py
+++ TestDataCrawlerByStep.py
@@ -6,7 +6,8 @@ from bs4 import BeautifulSoup
 
 # Get basic players information for all players
 base_url = "https://sofifa.com/players?offset="
-columns = ['Name', 'Nationality']
+columns = ['PlayerID', 'Name', 'Nationality', 'age', 'Photo', 'Flag', 'Overall', 'Potential', 'Club', 'Club Logo',
+           'Value', 'Wage', 'ReleaseClause']
 data = pd.DataFrame(columns=columns)
 
 for offset in range(0, 300):
@@ -16,12 +17,27 @@ for offset in range(0, 300):
     soup = BeautifulSoup(plain_text, 'html.parser')
     table_body = soup.find('tbody')
     for row in table_body.findAll('tr'):
+
         td = row.findAll('td')
-        nationality = td[1].find('a').get('title')
+
+        pid = td[6].text.strip()
         name = td[1].findAll('a')[1].text
-        player_data = pd.DataFrame([[name, nationality]])
+        nationality = td[1].find('a').get('title')
+        age = td[2].text.strip()
+        picture = td[0].find('img').get('data-src')
+        flag_img = td[1].find('img').get('data-src')
+        overall = td[3].text.strip()
+        potential = td[4].text.strip()
+        club = td[5].find('a').text
+        club_logo = td[5].find('img').get('data-src')
+        value = td[7].text.strip()
+        wage = td[8].text.strip()
+        release_clause = td[9].text.strip()
+
+        player_data = pd.DataFrame([[pid, name, nationality, age, picture, flag_img, overall, potential, club,
+                                     club_logo, value, wage, release_clause]])
+
         player_data.columns = columns
         data = data.append(player_data, ignore_index=True)
 data = data.drop_duplicates()
-
 data.to_csv('data.csv', encoding='utf-8-sig')
